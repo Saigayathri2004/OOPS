f2d94516a94148aa823a75599bca4219ee6db77c
Sai Gayathri J <saigargi2004@gmail.com>
2025-02-12 11:58:29 +0530

    Update Product.java

--- Product.java
+++ Product.java
@@ -39,3 +39,29 @@ obj2.display();
 obj3.display();
 Lowest(obj1.price,obj2.price,obj3.price);
 }}
+/*Algorithm:
+
+    Define the Product Class:
+        Create a class Product that has three attributes:
+            pcode: An integer to store the product code.
+            pname: A string to store the product name.
+            price: A double to store the product price.
+
+    Create a Method to Initialize Product Data:
+        Define a method data(int c, String n, double p) to set the values for the product code (pcode), product name (pname), and price (price).
+
+    Create a Method to Display Product Information:
+        Define a method display() that prints the product code, product name, and price.
+
+    Create a Static Method to Compare Prices:
+        Define a static method Lowest(double price1, double price2, double price3) that takes the prices of three products as input and determines which product has the lowest price:
+            If price1 is less than both price2 and price3, print "product1 has the lowest cost".
+            If price2 is less than both price1 and price3, print "product2 has the lowest cost".
+            Otherwise, print "product3 has the lowest cost".
+
+    In the Main Method:
+        Create three objects obj1, obj2, and obj3 of type Product.
+        Call the data method on each object to set the product details.
+        Print the table headers: "product code", "product name", and "price".
+        Display the details of each product using the display method.
+        Call the Lowest method with the prices of the three products to find out which one is the cheapest*/
