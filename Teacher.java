39e613e5628df47e3004a4c3d8b1d2c6d26dbbe0
Sai Gayathri J <saigargi2004@gmail.com>
2025-03-26 11:44:58 +0530

    Update Teacher.java

--- Teacher.java
+++ Teacher.java
@@ -1,112 +1,147 @@
-// Create a class ‘Person’ with data members Name, Gender, Address, Age and a constructor 
-// to initialize the data members  
+import java.util.*;
 
-import java.util.Scanner;
- class person {
-    String Name;
-    String Gender;
-    String Address;
-    int Age;
-    person(String name,String gender,String address, int age) {
-        this.Name = name;
-        this.Gender = gender;
-        this.Address = address;
-        this.Age = age;
+class Person {
+    String name;
+    String gender;
+    String address;
+    int age;
+
+    Person(String name, String gender, String address, int age) {
+        this.name = name;
+        this.gender = gender;
+        this.address = address;
+        this.age = age;
     }
 }
-//another class ‘Employee’ that inherits the properties of class Person and also contains its own data members
-// like Empid, Company_name, Qualification, Salary and its own constructor.
-class Employee extends person
-{
- int Empid;
- String Company_name;
- String Qualification;
- long Salary;
- 
- Employee(String name,String gender,String address, int age,int empid, String company_name, String qualification,long salary)
- {
-     super(name,gender,address,age);
-     this.Empid= empid;
-     this.Company_name=company_name;
-     this.Qualification=qualification;
-     this.Salary=salary;
- }
+
+class Employee extends Person {
+    int emp_id;
+    String c_name;
+    String quali;
+    int salary;
+
+    Employee(String name, String gender, String address, int age, int emp_id, String c_name, String quali, int salary) {
+        
+        super(name, gender, address, age);
+        this.emp_id = emp_id;
+        this.c_name = c_name;
+        this.quali = quali;
+        this.salary = salary;
+    }
+
+    void displayEmployeeDetails() {
+        System.out.println("Employee ID: " + emp_id);
+        System.out.println("Company Name: " + c_name);
+        System.out.println("Qualification: " + quali);
+        System.out.println("Salary: " + salary);
+    }
 }
-// Create another class ‘Teacher’ that inherits  the properties of class Employee and contains its 
-// own data members like Subject,Department, Teacherid and also contain constructors and methods to 
-// display the data members. Use array of objects to display details of N teachers.
- public class Teacher extends Employee{
-     String Subject;
-     String Department;
-     String Teacherid;
-    Teacher(String name,String gender,String address, int age,int empid, String company_name, String qualification,long salary, String subject, String department, String teacherid){
-        super(name,gender,address,age,empid,company_name,qualification,salary);
-        this.Subject=subject;
-        this.Department=department;
-        this.Teacherid=teacherid;
-    } 
 
-    void display(){
-        System.out.println("Name: "+Name);
-        System.out.println("Gender: "+Gender);
-        System.out.println("Address: "+Address);
-        System.out.println("Age: "+Age);
-        System.out.println("Employee id: "+Empid);
-        System.out.println("Company Name: "+Company_name);
-        System.out.println("Qualification: "+Qualification);
-        System.out.println("Salary: "+Salary);
-        System.out.println("Subject: "+Subject);
-        System.out.println("Department: "+Department);
-        System.out.println("Teacher id: "+Teacherid);
+class Teacher1 extends Employee {
+    String subject;
+    String depart;
+    int t_id;
 
-     }
-     public static void main(String[] args) {
-        System.out.println("\nEnter the No. of Teacher's");
+    Teacher1(String name, String gender, String address, int age, int emp_id, String c_name, String quali, int salary, String subject, String depart, int t_id) {
+       
+        super(name, gender, address, age, emp_id, c_name, quali, salary);
+        this.subject = subject;
+        this.depart = depart;
+        this.t_id = t_id;
+    }
+
+   
+    void display() {
+       
+        System.out.println("Name: " + name);
+        System.out.println("Gender: " + gender);
+        System.out.println("Address: " + address);
+        System.out.println("Age: " + age);
+        displayEmployeeDetails(); 
+        System.out.println("Subject: " + subject);
+        System.out.println("Department: " + depart);
+        System.out.println("Teacher ID: " + t_id);
+    }
+}
+ class Teacher {
+    public static void main(String[] args) {
+        int n;
+        Scanner sc = new Scanner(System.in);
         Scanner sc1 = new Scanner(System.in);
-        int num = sc1.nextInt();
-        Teacher arr[]=new Teacher[num];
-        System.out.println("\n Enter the Teacher Details\n");
-        int x = 0,j=0;
-        Scanner sc =new Scanner(System.in);
-        for(int i =0;i<num;i++)
-        {
-            x = i +1;
-            System.out.println("\n"+x+").");
-            System.out.println("\n Name: ");
-            String a =sc.next();
-            System.out.println("\n Gender: ");
-            String b =sc.next();
-            System.out.println("\n Address: ");
-            String c =sc.next();
-            System.out.println("\n Age: ");
-            int d =sc.nextInt();
-            System.out.println("\n Employee id: ");
-            int e =sc.nextInt();
-            System.out.println("\n Company name: ");
-            String f =sc.next();
-            System.out.println("\n Qualification: ");
-            String g =sc.next();
-            System.out.println("\n Salary: ");
-            long h =sc.nextLong();
-            System.out.println("\n Subject: ");
-            String k =sc.next();
-            System.out.println("\n Department: ");
-            String l =sc.next();
-            System.out.println("\n Teacher Id: ");
-            String n =sc.next();
-            arr[i]=new Teacher(a,b,c,d,e,f,g,h,k,l,n);
+
+        System.out.println("Enter the number of teachers: ");
+        n = sc.nextInt();      
+        Teacher1[] teachers = new Teacher1[n];
+
+        for (int i = 0; i < n; i++) {
+            System.out.println("Enter details for Teacher " + (i + 1));
+
+            System.out.println("Enter name of the teacher: ");
+            String name = sc1.nextLine();
+            System.out.println("Enter gender of the teacher: ");
+            String gender = sc1.nextLine();
+            System.out.println("Enter address of the teacher: ");
+            String address = sc1.nextLine();
+            System.out.println("Enter age of the teacher: ");
+            int age = sc.nextInt();
+            System.out.println("Enter employee ID: ");
+            int emp_id = sc.nextInt();
+            System.out.println("Enter company name: ");
+            String c_name = sc1.nextLine();  
+            c_name = sc1.nextLine();
+            System.out.println("Enter qualification: ");
+            String quali = sc1.nextLine();
+            System.out.println("Enter salary: ");
+            int salary = sc.nextInt();
+            System.out.println("Enter subject: ");
+            String subject = sc1.nextLine();
+            subject = sc1.nextLine();
+            System.out.println("Enter department: ");
+            String depart = sc1.nextLine();
+            System.out.println("Enter teacher ID: ");
+            int t_id = sc.nextInt();
+
+            teachers[i] = new Teacher1(name, gender, address, age, emp_id, c_name, quali, salary, subject, depart, t_id);
         }
-        sc.close();
-        System.out.println("\n********Informations of all the Teacher's************");
-        for(int i=0;i<num;i++){
-            j=i+1;
-            System.out.println("\n"+j+").");
-            arr[i].display();
-            
-     }
-     sc1.close();
-     }
-    
- }
+
+        for (int i = 0; i < n; i++) {
+            System.out.println("\nDetails of Teacher " + (i + 1) + ":");
+            teachers[i].display();  
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
